116d77ceb811e119bd63cb24a03667e63de17ac6
jamillessn <jamillerosesison1@gmail.com>
2024-01-27 11:13:53 +0800

    added code

--- OOP/Abstraction.rb
+++ OOP/Abstraction.rb
@@ -1,15 +1,20 @@
 class BankAccount
+    INTEREST_RATE = 0.8
     def initialize(balance)
         @balance = balance
     end
 
-    def interest(rate)
-        interest = (@balance * rate) / 100
+    def display_bal
+        puts "Current balance is #{@balance}"
+    end
+    def interest
+        interest = (@balance * INTEREST_RATE) / 100
         @balance += interest
         puts "New balance is #{@balance}"
     end
 end
 
 savings_acct = BankAccount.new(1000)
-savings_acct.interest(8)
+savings_acct.display_bal
+savings_acct.interest
 
