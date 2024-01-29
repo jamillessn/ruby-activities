32ed395f65e21eda2a4a6c3f41e24b6156fad6fe
jamillessn <jamillerosesison1@gmail.com>
2024-01-29 20:37:12 +0800

    first sql query commit

---
+++ SQL/main.sql
@@ -0,0 +1,38 @@
+
+CREATE TABLE students (
+	id 				integer 			PRIMARY KEY, 
+	first_name 		VARCHAR(25)   		NOT NULL,
+	middle_name		VARCHAR(100)		NULL,
+	last_name		VARCHAR(100)		NOT NULL,
+	age				INTEGER				NOT NULL,
+	location		VARCHAR(100)		NOT NULL
+);
+
+INSERT INTO
+    students (id, first_name, last_name, age, location)
+VALUES
+    (1, 'Juan', 'Cruz', 18, 'Manila');
+INSERT INTO
+    students (id, first_name, last_name, age, location)
+VALUES
+    (2, 'Anne', 'Wall', 20, 'Manila');
+INSERT INTO
+    students (id, first_name, last_name, age, location)
+VALUES
+    (3, 'Theresa', 'Joseph', 21, 'Manila');
+INSERT INTO
+    students (id, first_name, last_name, age, location)
+VALUES
+    (4, 'Isaac', 'Gray', 18, 'Laguna');
+INSERT INTO
+    students (id, first_name, last_name, age, location)
+VALUES
+    (5, 'Zack', 'Mathews', 18, 'Marikina');
+INSERT INTO
+    students (id, first_name, last_name, age, location)
+VALUES
+    (6, 'Finn', 'Lam', 18, 'Manila');
+    
+UPDATE students SET first_name ='Ivan', middle_name ='Ingram', last_name ='Howard', age = 25, location ='Bulacan'WHERE id = 1;
+
+DELETE FROM students WHERE id=6;
