d0df4e10c14ea909a231834f835e8eb0d8b8ddbb
jamillessn <jamillerosesison1@gmail.com>
2024-02-20 20:01:37 +0800

    corrected decode

--- the_vowel_code.rb
+++ the_vowel_code.rb
@@ -1,5 +1,3 @@
-
-
 def encode(input)
     vowels = {'a'=>1, 'e'=>2, 'i'=>3, 'o'=>4, 'u'=>5}
     result = ''  
@@ -15,15 +13,17 @@ def encode(input)
 end
 
 def decode(input)
-    vowels = {'a'=>1, 'e'=>2, 'i'=>3, 'o'=>4, 'u'=>5}
+    vowels = {'1'=>'a', '2'=>'e', '3'=>'i', '4'=>'o', '5'=>'u'}
     result = ''
+    
     input.each_char do |char|
-        if vowels.values.include?(char) 
-            result += vowels.value(char)
-        else 
+        if vowels.has_key?(char)
+            result += vowels[char].to_s
+        else
             result += char
-        end  
+        end
     end
+    
     p result
 end
 
